e5563574ea4f2d5cec74a861a2bc78eae0f7ce79
Tushar rout <trrout33@gmail.com>
2021-11-11 12:48:51 +0530

    first github commit 01

---
+++ Tushar_git/src/test/java/com/git/Deomgit1.java
@@ -0,0 +1,13 @@
+package com.git;
+
+import org.testng.annotations.Test;
+
+public class Deomgit1 {
+	@Test
+	
+	public void demogit1() {
+		
+		System.out.println("hi this is my first git commit");
+	}
+
+}
